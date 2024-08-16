9aa8ac874f9be7e6ef7d1fafbf1d10db81c92bec
hloh42 <hloh@student.42singapore.sg>
2024-08-16 21:10:11 +0800

    2nd Lesson with Doug

---
+++ doug.js
@@ -0,0 +1,19 @@
+document.querySelector("#doug").innerHTML = `
+    <ul>
+        <li>
+            <button>
+                <img src="https://picsum.photos/seed/1289374190827/50/50">
+            </button>
+        </li>
+        <li>
+            <button>
+                <img src="https://picsum.photos/seed/1289374190827/50/50">
+            </button>
+        </li>
+        <li>
+            <button>
+                <img src="https://picsum.photos/seed/1289374190827/50/50">
+            </button>
+        </li>
+    </ul>
+`
